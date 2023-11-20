d783852fb26fe0792b952ab315d24d88dec666ba
watanukitomonari <t.watanuki.sys23@morijyobi.ac.jp>
2023-11-20 21:45:32 +0900

    bmiの修正

--- bmi.java
+++ bmi.java
@@ -1,48 +1,42 @@
 import java.util.Scanner;
 
-public class Main {
+public class Bmi {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("BMI計算ツールです。(成人版)");
+	for(int i = 1 ; i < 3 ; i++) {
+		System.out.println( i + "人目の入力です。");
+        	System.out.println("身長を入力してください。(cm)");
+        	double height = Double.parseDouble(sc.nextLine()) / 100;
+        	System.out.println("体重を入力してください。(kg)");
+        	double weight = Double.parseDouble(sc.nextLine());
 
-        for (int i = 1; i <= 2; i++) { 
-            System.out.println("身長（メートル単位）を入力してください（Person " + i + "）：");
-            double height = scanner.nextDouble();
-
-            System.out.println("体重（キログラム単位）を入力してください（Person " + i + "）：");
-            double weight = scanner.nextDouble();
-
-            double bmi = calculateBMI(height, weight);
-            System.out.printf("Person " + i + " のBMIは %.2f です。\n", bmi);
-
-            String message = interpretBMI(bmi);
-            System.out.println(message);
-        }
-    }
-
-    public static double calculateBMI(double height, double weight) {
-        return weight / (height * height);
-    }
-
-    public static String interpretBMI(double bmi) {
-        String category;
-        if (bmi < 18.5) {
-            category = "過体重不足";
-        } else if (bmi >= 18.5 && bmi < 24.9) {
-            category = "適正体重";
-        } else if (bmi >= 24.9 && bmi < 29.9) {
-            category = "過体重";
-        } else {
-            category = "肥満";
-        }
-
-        String message = "あなたのBMIは " + String.format("%.2f", bmi) + " で、" + category + " です。\n";
-
-        if (category.equals("適正体重")) {
-            message += "健康的な状態です。継続して健康な生活を維持しましょう。";
-        } else {
-            message += "適正体重を目指すために、食事と運動の改善を検討しましょう。";
-        }
-
-        return message;
+        	double BMI = weight / (height * height);
+        	BMI = (Math.floor(BMI * 100)) / 100;
+        	double tekisei = ((Math.floor((height * height) * 10)) * 22) / 10;
+        	System.out.println("あなたのBMIは" + BMI + "です。");
+        	System.out.println("あなたの適正体重は" + tekisei + "kgです。");
+        	System.out.print("あなたの肥満度は");
+        	if(BMI < 18.5) {
+        	    System.out.println("低体重(やせ型)です。");
+        	    System.out.println("もっと飯食いましょう");
+        	} else if (BMI > 18.5 && BMI < 25) {
+        	    System.out.println("普通体重です。");
+        	    System.out.println("健康です。");
+        	} else if (BMI > 25 && BMI < 30) {
+        	    System.out.println("肥満(1度)です。");
+        	    System.out.println("痩せましょう");
+        	} else if (BMI > 30 && BMI < 35) {
+        	    System.out.println("肥満(2度)です。");
+        	    System.out.println("痩せましょう");
+        	} else if (BMI > 35 && BMI < 40) {
+        	    System.out.println("肥満(3度)です。");
+        	    System.out.println("痩せましょう");
+        	} else if (BMI > 40) {
+        	    System.out.println("肥満(4度)です。");
+        	    System.out.println("痩せましょう");
+        	}
+		System.out.println("----------------------------");
+	}
     }
 }
